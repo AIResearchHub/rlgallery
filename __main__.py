343910d8dd3cb339b690b34f8ccbe8355e8acf66
YHL04 <22limyihu@gmail.com>
2023-06-26 09:57:33 -0400

    first commit

--- __main__.py
+++ __main__.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from environment import AtariEnv
+from agents import Random
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main(env_name="BreakoutDeterministic-v4",
+         total_timesteps=100_000_000
+         ):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    env = AtariEnv(env_name)
+    agent = Random()
+
+    env.reset()
+
+    done = False
+    while not done:
+        env.step(agent.get_action())
+
